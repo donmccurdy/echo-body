24d0defa8c66ee6909ea3043b81a3f96e6ae26d8
Don McCurdy <donmccurdy@google.com>
2015-12-07 16:30:01 -0500

    v1.0.4, remove inaccurate referrer.

--- echo-body.js
+++ echo-body.js
@@ -7,7 +7,7 @@ var koa = require('koa'),
 
 // Bind CLI interface.
 program
-	.version('1.0.3')
+	.version('1.0.4')
 	.description('Listens on specified port and echos request bodies.')
 	.option('-p, --port', 'Port', Number)
 	.parse(process.argv);
@@ -19,8 +19,7 @@ program.port = program.port || 4000;
 koa()
 	.use(bodyParser({multipart: true}))
 	.use(function *() {
-		console.log(chalk.green('Request from %s'), this.request.header.host);
-		console.log(chalk.yellow(' - URL: ') + this.request.url);
+		console.log(chalk.green('Request to %s'), this.request.header.host + this.request.url);
 		console.log(chalk.yellow(' - Method: ') + this.request.method);
 		console.log(chalk.yellow(' - Body: ') + JSON.stringify(this.request.body, null, 2));
 		this.status = 200;
