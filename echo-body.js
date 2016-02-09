15552a2426c2ba3211a957177b34b0dc4470f355
Don McCurdy <don.r.mccurdy@gmail.com>
2016-02-09 17:46:25 -0500

    Show content type.

--- echo-body.js
+++ echo-body.js
@@ -13,12 +13,18 @@ program
 	.option('-p, --port <port>', 'Port', Number, process.env.PORT || 4000)
 	.parse(process.argv);
 
+var CONTENT_TYPES = [
+	'json', 'urlencoded', 'text', 'multipart'
+];
+
 // Launch echo server.
 koa()
 	.use(bodyParser({multipart: true}))
 	.use(function *() {
+		var contentType = CONTENT_TYPES.filter((type) => this.is(type))[0] || '?';
 		console.log(chalk.green('Request to %s'), this.request.header.host + this.request.url);
 		console.log(chalk.yellow(' - Method: ') + this.request.method);
+		console.log(chalk.yellow(' - Content-Type: ') + contentType);
 		console.log(chalk.yellow(' - Body: ') + JSON.stringify(this.request.body, null, 2));
 		this.status = 200;
 	})
